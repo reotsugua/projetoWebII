a1e63ee74d9c8a28dbdb7ab89704d259cc7105d4
reotsugua <renanaugusto.ras@gmail.com>
2023-03-31 20:28:29 -0300

    primeiro commit

---
+++ webll/src/main/java/br/com/projeto/webll/WebllApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.webll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebllApplication.class, args);
+	}
+
+}
